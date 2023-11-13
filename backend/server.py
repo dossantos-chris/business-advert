c7e5ad867de8bbf692f5a2b85547e2a4c862769f
Chris Dos Santos <dossantos.chris13@gmail.com>
2023-11-13 16:41:02 -0500

    Init Pymongo and MongoDB integration, tested basic functionality

--- backend/server.py
+++ backend/server.py
@@ -1,16 +1,19 @@
 from flask import Flask, jsonify
+from pymongo import MongoClient
 from flask_cors import CORS
 import os
-from dotenv import load_dotenv, dotenv_values
+from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
+client = MongoClient(os.getenv("MONGO_URI"))
+db = client.BusinessAdvert
 CORS(app)
 
 @app.route("/test", methods=["GET"])
-def test():
-    test = os.getenv("API_KEY")
-    return jsonify({'test': f'{test}'})
+async def test():
+    business = db['businesses'].find_one({"name" : "Xina"}, {"_id": 0})  # Exclude _id field
+    return jsonify({'business': business})
 
 if __name__ == "__main__":
     app.run(debug=True)

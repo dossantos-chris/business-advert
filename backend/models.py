efaa7d442e2d49264eb0e605fdebca6a30855880
Chris Dos Santos <dossantos.chris13@gmail.com>
2023-11-14 16:06:56 -0500

    Implemented current date and time field for when business is added

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+
 class Business:
     def __init__(self, name, service, city, state):
         self.name = name
         self.service = service
         self.city = city
         self.state = state
+        self.dateAdded = str(datetime.now())

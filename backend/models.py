bd97fcebdf882ab7e318e7364fa3c97c8b9d576e
Chris Dos Santos <dossantos.chris13@gmail.com>
2023-11-14 16:00:35 -0500

    Implemented functionality to add a business to the db and created business model

---
+++ backend/models.py
@@ -0,0 +1,6 @@
+class Business:
+    def __init__(self, name, service, city, state):
+        self.name = name
+        self.service = service
+        self.city = city
+        self.state = state
--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+from models import Business
 from bson import ObjectId
 from motor.motor_asyncio import AsyncIOMotorClient
 from flask_cors import CORS
@@ -9,7 +10,7 @@ load_dotenv()
 app = Flask(__name__)
 client = AsyncIOMotorClient(os.getenv("MONGO_URI"))
 db = client.BusinessAdvert
-CORS(app)
+CORS(app) 
 
 @app.route("/list", methods=["GET"])
 async def list():
@@ -23,7 +24,21 @@ async def list():
 
 @app.route("/add", methods=["POST"])
 async def add():
-    return "test", 200
+    data = request.json
+
+    if 'name' in data and 'service' in data and 'city' in data and 'state' in data:
+        new_business = Business(data['name'], data['service'], data['city'], data['state'])
+
+        business_dict = vars(new_business)
+
+        result = await db.businesses.insert_one(business_dict)
+
+        if result.inserted_id:
+            return jsonify({'message': 'Document created successfully.', '_id': str(result.inserted_id)}), 201
+        else:
+            return jsonify({'error': 'Failed to create document.'}), 500
+    else:
+        return jsonify({'error': 'Invalid business model'}), 400
 
 @app.route("/delete", methods=['DELETE'])
 async def delete():
@@ -37,12 +52,12 @@ async def delete():
     except:
         return jsonify({"error": "Not a valid business ID"}) , 400
 
-    business = await db.businesses.delete_one({"_id": oid})
+    result = await db.businesses.delete_one({"_id": oid})
 
-    if business.deleted_count == 0:
+    if result.deleted_count == 0:
         return jsonify({"error": "Business not found"}) , 404
 
-    return jsonify({"deleted": business_id}), 200
+    return jsonify({"message": "Business deleted successfully", '_id': business_id}), 200
 
 if __name__ == "__main__":
     app.run(debug=True)

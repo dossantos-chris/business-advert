b30d78995915332320ebcfed35c7c6bc7c16216f
Chris Dos Santos <dossantos.chris13@gmail.com>
2023-11-14 15:27:01 -0500

    Implemented delete functionality for business DB

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from bson import ObjectId
 from motor.motor_asyncio import AsyncIOMotorClient
 from flask_cors import CORS
 import os
@@ -10,16 +11,38 @@ client = AsyncIOMotorClient(os.getenv("MONGO_URI"))
 db = client.BusinessAdvert
 CORS(app)
 
-@app.route("/", methods=["GET"])
-async def home():
-    return "<main>Home Page</main>"
-
-
-@app.route("/test", methods=["GET"])
-async def test():
-    businessCursor = db.businesses.find({}, {"_id": 0})
+@app.route("/list", methods=["GET"])
+async def list():
+    businessCursor = db.businesses.find({})
     businesses = await businessCursor.to_list(None)
-    return jsonify({"businesses": businesses})
+
+    for business in businesses:
+        business['_id'] = str(business['_id'])
+
+    return jsonify({"businesses": businesses}), 200
+
+@app.route("/add", methods=["POST"])
+async def add():
+    return "test", 200
+
+@app.route("/delete", methods=['DELETE'])
+async def delete():
+    business_id = request.args.get('id', None)
+
+    if business_id is None:
+        return jsonify({"error": "Must include a business ID"}) , 400
+
+    try:
+        oid = ObjectId(business_id)
+    except:
+        return jsonify({"error": "Not a valid business ID"}) , 400
+
+    business = await db.businesses.delete_one({"_id": oid})
+
+    if business.deleted_count == 0:
+        return jsonify({"error": "Business not found"}) , 404
+
+    return jsonify({"deleted": business_id}), 200
 
 if __name__ == "__main__":
     app.run(debug=True)

87d9cb37b8bfd6c8c47e83a055b2ee4dca89339a
Chris Dos Santos <dossantos.chris13@gmail.com>
2023-11-15 17:29:04 -0500

    Set up CORS with FastAPI backend

--- backend/app/server/app.py
+++ backend/app/server/app.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware import Middleware
 
 from app.server.routes.business import router as BusinessRouter
 
+#probably set up an origin list at some point
 app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 #maybe add tags to this line and routes for documentation
 app.include_router(BusinessRouter, prefix="/business")
+

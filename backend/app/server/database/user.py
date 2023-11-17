ff0aaa8e90d0313927fe9442c1fec33eeb99af07
Chris Dos Santos <dossantos.chris13@gmail.com>
2023-11-16 22:06:54 -0500

    Implemented fetching of users from users collection

--- backend/app/server/database/user.py
+++ backend/app/server/database/user.py
@@ -1,3 +1,18 @@
 from app.server.db import db
+from app.server.models.user import UserInDBSchema
 
 user_collection = db.users
+
+def user_helper(user) -> dict:
+    result = {
+        "username": user["username"],
+        "hashed_password": user["hashed_password"],
+        "disabled": user["disabled"]
+    }
+
+    return result
+
+async def get_user(username: str):
+    user = await user_collection.find_one({"username": username})
+    if user:
+        return UserInDBSchema(**user_helper(user))
